fae0b7b1baacc842a8e1b0094ef76edc0476ee45
MarAviles <mariana.avilesmartinez@gmail.com>
2022-07-15 22:33:48 -0500

    Actualización db

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,11 @@ import { ProductoModule } from './components/producto/producto.module';
 @Module({
   imports: [ProductoModule, TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'localhost',
+    host: 'ec2-44-197-128-108.compute-1.amazonaws.com',
     port: 5432,
-    username: 'postgres',
-    password: 'Legendary_132206',
-    database: 'ecommerce',
+    username: 'gxrqqyyovlswww',
+    password: 'b083501040c4b5a78c7c5f79b86c345c3877b9300116b56c8489020e162906bb',
+    database: 'd5t8pdtttq8t64',
     entities: [Producto],
     autoLoadEntities: true,
     synchronize: true
