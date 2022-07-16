7acd8ce56911eb507ddb11e4bf37d357a0e8ce8a
MarAviles <83610210+MarAviles@users.noreply.github.com>
2022-07-16 02:12:17 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Producto } from './components/producto/entity/producto.entity';
 
 import { ProductoModule } from './components/producto/producto.module';
 
 @Module({
-  imports: [ProductoModule, ConfigModule.forRoot(), TypeOrmModule.forRoot({
-      url: process.env.DATABASE_URL,
-      type: 'postgres',
-      ssl: {
-        rejectUnauthorized: false,
-      },
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true, // This for development
-      autoLoadEntities: true,
+  imports: [ProductoModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'ec2-3-217-14-181.compute-1.amazonaws.com',
+    port: 5432,
+    username: 'dldgtwurfjgvqg',
+    password: 'ad444505e033da3763c1acf89d1870b5b71b768399f44867f9404b13462a1532',
+    database: 'd2p3bde51uejl1',
+    entities: [Producto],
+    autoLoadEntities: true,
+    synchronize: true
   })],
   controllers: [],
   providers: [],
